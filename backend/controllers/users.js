6a8f7875801824129ec7e5948ca44b6668268acf
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-07-14 12:26:30 +0200

    fix: delete useless routes

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -92,25 +92,6 @@ module.exports.logout = (req, res, next) => {
     .catch(next);
 };
 
-module.exports.getUsers = (req, res, next) => {
-  User.find({})
-    .then((users) => res.send(users))
-    .catch(next);
-};
-
-module.exports.getUser = (req, res, next) => {
-  User.findById(req.params.userId)
-    .orFail(() => new NotFoundError('Пользователь с указанным _id не найден.'))
-    .then((user) => res.send(user))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new BadRequestError('Переданы некорректные данные при запросе пользователя.'));
-        return;
-      }
-      next(err);
-    });
-};
-
 module.exports.getMe = (req, res, next) => {
   User.findById(req.user._id)
     .orFail(() => new NotFoundError('Пользователь с указанным _id не найден.'))
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,22 +1,13 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const {
-  getUsers,
-  getUser,
   getMe,
   updateUserInfo,
   updateAvatar,
 } = require('../controllers/users');
 const { linkRegex } = require('../utils/constants');
 
-router.get('/', getUsers);
 router.get('/me', getMe);
-
-router.get('/:userId', celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24),
-  }),
-}), getUser);
 
 router.patch('/me', celebrate({
   body: Joi.object().keys({
